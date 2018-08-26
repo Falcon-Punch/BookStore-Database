cf3cede9e6c334b9b5bc3c1888627b4f7c789f3b
Joseph Schell <JosephSchell510@gmail.com>
2018-08-26 00:47:57 -0700

    Created insert and view all functions to backend.

--- backend.py
+++ backend.py
@@ -7,4 +7,21 @@ def connect():
     conn.commit()
     conn.close()
 
+def insert(title, author, year, isbn):
+    conn=sqlite3.connect("bookList.db")
+    curr=conn.cursor()
+    curr.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("bookList.db")
+    curr=conn.cursor()
+    curr.execute("SELECT * FROM book")
+    rows=curr.fetchall()
+    conn.close()
+    return rows
+
 connect()
+insert("Decoding the Coding Interview", "John Doe", 2015, 123456789)
+print(view())
