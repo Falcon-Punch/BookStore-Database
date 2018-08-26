f1ad7bb2cf46076a94a38375f37003a3bc372187
Joseph Schell <JosephSchell510@gmail.com>
2018-08-25 23:57:43 -0700

    Added List box and buttons to UI.

--- script.py
+++ script.py
@@ -30,4 +30,31 @@ isbn_text=StringVar()
 entry4=Entry(window, textvariable=isbn_text)
 entry4.grid(row=1, column=3)
 
+list1=Listbox(window, height=10, width=52)
+list1.grid(row=3, column=0, rowspan=7, columnspan=4)
+
+scrollBar=Scrollbar(window)
+scrollBar.grid(row=3, column=4, rowspan=6)
+
+list1.configure(yscrollcommand=scrollBar.set)
+scrollBar.configure(command=list1.yview)
+
+button1=Button(window, text="View All", width=12)
+button1.grid(row=3, column=5)
+
+button1=Button(window, text="Search Entry", width=12)
+button1.grid(row=4, column=5)
+
+button1=Button(window, text="Add Entry", width=12)
+button1.grid(row=5, column=5)
+
+button1=Button(window, text="Update", width=12)
+button1.grid(row=6, column=5)
+
+button1=Button(window, text="Delete Entry", width=12)
+button1.grid(row=7, column=5)
+
+button1=Button(window, text="Close App", width=12)
+button1.grid(row=8, column=5)
+
 window.mainloop()
